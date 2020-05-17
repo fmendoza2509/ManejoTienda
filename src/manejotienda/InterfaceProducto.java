bc00cd1a2830dafc37dd35b1025ded94450012ba
frangusdj <frangusdj@hotmail.com>
2020-05-16 23:38:51 -0500

    los metodos que no se guardaron

--- src/manejotienda/InterfaceProducto.java
+++ src/manejotienda/InterfaceProducto.java
@@ -1,15 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package manejotienda;
-
-/**
- *
- * @author Franklin
- */
-public class InterfaceProducto {
-
+public abstract class InterfaceProducto {
+    /*********************************************
+     * Método determinar si la tienda esta vacia *
+     * retorna un true, caso contrario retorna   *
+     * false                                     *
+     *********************************************/
+    public abstract boolean EstaVacio();//Requerimeinto no funcional
+    /*********************************************
+     * Método para determinar la cantidad de los *
+     * productos de la tienda @return int        *
+     *********************************************/
+    public abstract int Tamano();//Requerimiento no funcional
+    /********************************************
+     * Método para vaciar todos los productos de*
+     * la tienda                                *
+     ********************************************/
+    public abstract void Vaciar();//Requerimiento no funcional
+   /**************************************
+    * Método Agregar producto a la tienda*
+    **************************************/
+   public abstract boolean Agregar(Object elemento);//Requerimiento funcional
+   /********************************
+    * Método para eliminar producto*
+    ********************************/
+   public abstract boolean Eliminar(String codigo);//Requerimiento funcional
+   /*********************************************
+    *Metodo para buscar un producto en la tienda*
+    *********************************************/
+   public abstract boolean Buscar(String codigo);//Requerimiento funcional
+   /***************************************
+    * Método para actualizar los productos*
+    ***************************************/
+   public abstract boolean Editar(String cod, String nomb, String desc, int cant);//Requerimiento funcional
+   /*************************
+    * Método vender producto*
+    *************************/
+   public abstract void Vender(String cod, String nomb, String desc, int cant);//Requerimiento funcional
 }
---
+++ src/manejotienda/Productos.java
@@ -0,0 +1,4 @@
+package manejotienda;
+public class Productos implements InterfaceProducto{
+
+}
