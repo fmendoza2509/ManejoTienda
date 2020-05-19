5cb7ec5c3a5b0270a857f956d5e9beca41d32f22
joao17017 <joao17017@gmail.com>
2020-05-18 21:02:33 -0500

    Metodos setters y getter en la clase tienda

--- src/manejotienda/Tienda.java
+++ src/manejotienda/Tienda.java
@@ -1,16 +1,51 @@
 package manejotienda;
 public class Tienda {
-    private String nombre, direccion;
-    public Tienda(){
+    private String codigo, nombre, descripcion;
+    private int cantidad;
+
+    //constructores
+    public Tienda() {
     }
-    public Tienda(String _nomb, String _dir){
-        nombre = _nomb;
-        direccion = _dir;
+    public Tienda(String codigo, String nombre, String descripcion, int cantidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
     }
-    public void setNom(String nomb){
-        nombre = nomb;
+    //M{etodos Setters
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
     }
-    public void setDir(String dir){
-        direccion = dir;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    //M{etodos Getter
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    
 }
